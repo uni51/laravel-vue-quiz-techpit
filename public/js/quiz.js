2f1dde79477099efca5c6be78ca2266430ffc2fe
uni51 <uni51@users.noreply.github.com>
2020-06-30 15:30:49 +0900

    6-7 クイズ画面のクイズ出題API連携を実装する-Vue.js側の実装-2問目以降の問題を表示する

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -223,12 +223,54 @@ __webpack_require__.r(__webpack_exports__);
     });
   },
   methods: {
+    goAnswer: function goAnswer(selectAnswerNum) {
+      if (selectAnswerNum === 0) {
+        // selectAnswerNumが0の場合は、click 「正解を表示する」ボタンのクリック alert-info、alert-dangerを非表示
+        this.isCorrect = false;
+        this.isMistake = false;
+      } else if (selectAnswerNum === Number(this.correctAnswerNo)) {
+        // 正解を押した場合 alert-infoを表示し、alert-dangerを非表示にする そしてスコアを加算する
+        this.isCorrect = true;
+        this.isMistake = false;
+        this.score += 1;
+      } else {
+        // 不正解の場合 alert-infoを非表示し、alert-dangerを表示にする
+        this.isMistake = true;
+        this.isCorrect = false;
+      } // 回答済みの設定をONにする 同じ問題に２回以上の回答をさせないため、そして解説を表示するため
+
+
+      this.isAlreadyAnswered = true; // 10問以上回答している場合は、クイズを終了
+
+      if (this.quizNumber >= 10) {
+        this.endQuiz();
+      }
+    },
     findNextQuiz: function findNextQuiz(quizNumber) {
       this.title = this.quizData[quizNumber].title;
       this.answers = [this.quizData[quizNumber].answer.answer_1, this.quizData[quizNumber].answer.answer_2, this.quizData[quizNumber].answer.answer_3, this.quizData[quizNumber].answer.answer_4];
       this.commentary = this.quizData[quizNumber].answer.commentary;
       this.correctAnswerNo = this.quizData[quizNumber].answer.correct_answer_no;
       this.categoryName = this.quizData[quizNumber].category.name;
+    },
+    goNextQuiz: function goNextQuiz() {
+      // 次の問題へをクリック
+      if (this.quizNumber >= 10) {
+        // 10問以上の場合はクイズを終了
+        this.endQuiz();
+      } else {
+        // 次のクイズを表示し、クイズ番号を加算、alert-info、alert-danger、解説を非表示にする
+        this.findNextQuiz(this.quizNumber);
+        this.quizNumber += 1;
+        this.isCorrect = false;
+        this.isMistake = false;
+        this.isAlreadyAnswered = false;
+      }
+    },
+    endQuiz: function endQuiz() {
+      this.isQuizFinish = true;
+      this.answerNo = "-";
+      this.isAlreadyAnswered = true;
     }
   }
 });
