6f88785caef72915f82ba8d340427a1913f91b1e
uni51 <uni51@users.noreply.github.com>
2020-06-30 16:05:14 +0900

    7-2 実装準備Layoutの共通化

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import router from './router.js'
 import SocialSharing from 'vue-social-sharing'
 import axios from 'axios'
 import jQuery from "jquery";
+import MainPage from './components/page/MainPage'
+
 window.$ = window.jQuery = jQuery;
 require('bootstrap');
 
@@ -11,5 +13,8 @@ Vue.prototype.$http = axios;
 Vue.use(SocialSharing);
 
 new Vue({
-    router: router, // routerにはrouter.jsファイルを設定します
+  router: router, // routerにはrouter.jsファイルを設定します
+  components: {
+    app: MainPage
+  }
 }).$mount('#app') // routerを適用する要素を設定(マウント)します
--- resources/js/components/page/Home.vue
+++ resources/js/components/page/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <the-header></the-header>
-
     <main>
       <div class="container">
         <article class="col-md-8 col-xs-12">
@@ -58,26 +56,19 @@
             </dl>
           </section>
         </article>
-
         <the-sidebar></the-sidebar>
-
       </div>
     </main>
 
-    <the-footer></the-footer>
   </div>
 </template>
 
 <script>
-import TheHeader from "../layout/TheHeader";
-import TheFooter from "../layout/TheFooter";
 import TheSidebar from "../layout/TheSidebar";
 import BarChart from "../module/BarChart";
 
 export default {
   components: {
-    TheHeader,
-    TheFooter,
     TheSidebar,
     BarChart,
   },
