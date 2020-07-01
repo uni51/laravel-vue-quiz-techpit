1e5619227b8c6cfbb974944b63e56708cb10b765
uni51 <uni51@users.noreply.github.com>
2020-07-01 09:52:37 +0900

    8-4 ログイン・ログアウト機能の作成 -ログアウト機能の実装-

--- public/js/app.js
+++ public/js/app.js
@@ -1997,6 +1997,11 @@ __webpack_require__.r(__webpack_exports__);
     auth: {
       type: Object | Array
     }
+  },
+  methods: {
+    logout: function logout() {
+      document.querySelector("#logout-form").submit();
+    }
   }
 });
 
@@ -58149,7 +58154,8 @@ var render = function() {
                               "a",
                               {
                                 staticClass: "navbar-brand",
-                                attrs: { id: "header-nav__logout" }
+                                attrs: { id: "header-nav__logout" },
+                                on: { click: _vm.logout }
                               },
                               [_vm._v("ログアウト")]
                             ),
--- resources/js/components/layout/TheHeader.vue
+++ resources/js/components/layout/TheHeader.vue
@@ -50,7 +50,7 @@
               <ul class="dropdown-menu header-nav__drop-down" role="menu">
                 <li>
                   <router-link class="navbar-brand" id="header-nav__mypage" to="/mypage">マイページ</router-link>
-                  <a class="navbar-brand" id="header-nav__logout" >ログアウト</a>
+                  <a class="navbar-brand" id="header-nav__logout" @click="logout">ログアウト</a>
                   <form id="logout-form" action="/logout" method="POST" style="display: none;">
                     <input type="hidden" name="_token" :value="csrf" />
                   </form>
@@ -77,6 +77,11 @@
     props: {
       auth: {
         type: Object | Array
+      }
+    },
+    methods: {
+      logout() {
+        document.querySelector("#logout-form").submit();
       }
     }
   };
