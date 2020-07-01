1bedb33d6fa00fe573d4692b2b15f781e01a3479
uni51 <uni51@users.noreply.github.com>
2020-07-01 09:18:04 +0900

    8-1 ログイン・ログアウト機能の作成 -見た目のコーディングとVeeValidateの導入-

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Route::get('/', function () {
 Route::get('/quiz', function () {
     return view('quiz.index');
 });
+Route::get('/login', function () {
+    return view('auth.login.index');
+});
 Route::get('/register', function () {
     return view('auth.register.index');
 });
