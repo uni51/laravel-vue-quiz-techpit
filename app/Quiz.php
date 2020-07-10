250fd2565cbae706fcc701f2437b3d1997e6b8a8
uni51 <uni51@users.noreply.github.com>
2020-07-11 07:53:18 +0900

    11-3 Laravel-Adminのカスタマイズ -クイズ・回答の管理の実装-

--- app/Quiz.php
+++ app/Quiz.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
 use App\Answer;
 use App\Category;
+use Illuminate\Database\Eloquent\Model;
 
 class Quiz extends Model
 {
@@ -18,5 +18,21 @@ class Quiz extends Model
     public function category()
     {
         return $this->hasOne('App\Category', 'id', 'categories_id');
+    }
+
+    /**
+     *
+     * モデルのリレーションでQuizを削除したら、紐付くAnswerも削除する
+     * boot()は登録や削除の際に実行されるメソッドで、そこにLaravelのModelに標準で定義済みであるdeletingメソッドを追加し、
+     * リレーションで紐づいたAnswerを削除するように設置します。
+     *
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($answer_model) {
+            $answer_model->answer()->delete();
+        });
     }
 }
