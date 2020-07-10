a2c3eb1d94d4e445377f11f096b99675d23e98ee
uni51 <uni51@users.noreply.github.com>
2020-07-10 15:52:21 +0900

    10-1 クイズ画面の調整

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import router from './router.js'
 import SocialSharing from 'vue-social-sharing'
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 import axios from 'axios'
 import MainPage from './components/page/MainPage'
-
 global.jQuery = global.$ = require('jquery');
 require('bootstrap');
 
@@ -14,6 +15,7 @@ axios.defaults.headers.common['Authorization'] = "Bearer " + document
   .getAttribute("content");
 
 Vue.use(SocialSharing);
+Vue.use(Loading);
 
 new Vue({
   router: router, // routerにはrouter.jsファイルを設定します
--- resources/js/components/page/Quiz.vue
+++ resources/js/components/page/Quiz.vue
@@ -109,11 +109,24 @@
     },
     mounted() {
       const categories = this.$route.query.categories;
-      this.$http.get(`/api/quiz?categories=${categories}`).then(response => {
-        this.quizData = response.data;
-        this.findNextQuiz(0);
-        console.log(this.quizData);
-      });
+      const loader = this.$loading.show();
+      this.$http
+        .get(`/api/quiz?categories=${categories}`)
+        .then(response => {
+          this.quizData = response.data;
+          // 10問より少なければ強制的に初期画面戻す
+          if (this.quizData.length < 10) {
+            alert("クイズ10問以下のため、初期画面に戻ります。カテゴリーを選択し直してください");
+            location.href = "/";
+          } else {
+            this.findNextQuiz(0);
+            loader.hide();
+          }
+        })
+        .catch(error => {
+          alert("クイズの読み込みに失敗したため、初期画面に戻ります");
+          location.href = "/";
+        });
     },
     methods: {
       goAnswer(selectAnswerNum) {
@@ -141,6 +154,7 @@
       },
 
       findNextQuiz(quizNumber) {
+        window.scroll(0, 0);
         this.title = this.quizData[quizNumber].title;
         this.answers = [
           this.quizData[quizNumber].answer.answer_1,
